69fade5b881c695e2e1db1210d540605ca95256a
lorenzopasqualini <lorenzopasqualini@icloud.com>
2024-06-23 01:07:14 +0200

    separate components. SOC

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,17 @@
+<script setup>
+  import StationTable from './components/StationTable.vue';
+</script>
+
 <template>
   <div class="container">
-    <h1>Keonn Technologies BCN EV Stations Map</h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Address</th>
-          <th>Availability</th>
-          <th>Open hours</th>
-          <th>Number of stations</th>
-          <th>On street location</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in items" :key="item.id">
-          <td>{{ item.address.address_string }}</td>
-          <td>{{ item.access_restriction }}</td>
-
-          <td>
-            <li v-for="hours in item.opening_hours" :key="hours.id">
-              {{ hours.hour_begin }} - {{ hours.hour_end }}
-            </li>
-          </td>
-
-          <td>{{ item.stations.length }}</td>
-
-          <td>{{ item.onstreet_location ? '&#9989;' : '&#9940;' }}</td>
-
-        </tr>
-      </tbody>
-    </table>
+    <StationTable/>
+    <p>map component</p>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      items: []
-    };
-  },
-  mounted() {
-    axios.get('/data/loc.json')
-      .then(response => {
-        this.items = response.data.locations;
-        console.log(response.data.locations);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-};
-</script>
 
 <style scoped>
-li{
-  list-style-type: none;
-}
-td{
-  border: 0.5em solid red
-}
+ .container {
+    display: flex;
+  }
 </style>
---
+++ src/components/StationTable.vue
@@ -0,0 +1,60 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+            <th>Address</th>
+            <th>Availability</th>
+            <th>Open hours</th>
+            <th>Open days</th>
+            <th>Number of stations</th>
+            <th>On street location</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.id">
+            <td>{{ item.address.address_string }}</td>
+            <td>{{ item.access_restriction }}</td>
+            <td> <li v-for="hours in item.opening_hours" :key="hours.id">{{ hours.hour_begin }} - {{ hours.hour_end }}</li> </td>
+            <td> <li v-for="hours in item.opening_hours" :key="hours.id">{{ (hours.weekday_begin === 1 && hours.weekday_end === 7) ? 'All week' : 'Some weekdays' }}</li> </td>
+            <td>{{ item.stations.length }}</td>
+            <td>{{ item.onstreet_location ? '&#9989;' : '&#9940;' }}</td>
+            </tr>
+        </tbody>
+    </table>
+  </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  mounted() {
+    axios.get('/data/loc.json')
+      .then(response => {
+        this.items = response.data.locations;
+        console.log(response.data.locations);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+li{
+  list-style-type: none;
+}
+th, td{
+  border: 0.1em solid teal;
+  padding: 1em;
+  width: 100%;
+}
+thead{
+  background: teal;
+}
+</style>
