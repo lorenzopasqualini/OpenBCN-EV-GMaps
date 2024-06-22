504309c999d2de1721b500869499bf05c4d39429
lorenzopasqualini <lorenzopasqualini@icloud.com>
2024-06-23 00:41:46 +0200

    table format with relevant columns

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        <h5>{{ item }}</h5>
-      </li>
-    </ul>
+  <div class="container">
+    <h1>Keonn Technologies BCN EV Stations Map</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Address</th>
+          <th>Availability</th>
+          <th>Open hours</th>
+          <th>Number of stations</th>
+          <th>On street location</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.address.address_string }}</td>
+          <td>{{ item.access_restriction }}</td>
+
+          <td>
+            <li v-for="hours in item.opening_hours" :key="hours.id">
+              {{ hours.hour_begin }} - {{ hours.hour_end }}
+            </li>
+          </td>
+
+          <td>{{ item.stations.length }}</td>
+
+          <td>{{ item.onstreet_location ? '&#9989;' : '&#9940;' }}</td>
+
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -31,4 +55,10 @@ export default {
 </script>
 
 <style scoped>
+li{
+  list-style-type: none;
+}
+td{
+  border: 0.5em solid red
+}
 </style>
