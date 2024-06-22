b393b4685277ba7e7c2ab444504d87bf2bb57447
lorenzopasqualini <lorenzopasqualini@icloud.com>
2024-06-22 23:50:24 +0200

    render json through axios

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h5>{{ item }}</h5>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  mounted() {
+    axios.get('/data/loc.json')
+      .then(response => {
+        this.items = response.data.locations;
+        console.log(response.data.locations);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+</style>
